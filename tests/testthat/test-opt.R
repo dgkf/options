631583398b483074a597a5be0a1af41a2e17346f
dgkf <18220321+dgkf@users.noreply.github.com>
2024-01-27 22:33:57 -0500

    adding a couple more tests

--- tests/testthat/test-opt.R
+++ tests/testthat/test-opt.R
@@ -27,9 +27,24 @@ test_that("opts() can be used to set options when provided a named list", {
   expect_equal(o$verbose, TRUE)
 })
 
+test_that("opts() can be used to retrieve options when provided a unnamed list", {
+  define_options("quietly", quiet = FALSE, "verbosity", verbose = FALSE)
+  on.exit(remove_options_env())
+
+  expect_silent(o <- opts(list("quiet", "verbose")))
+  expect_length(o, 2)
+  expect_named(o, c("quiet", "verbose"))
+  expect_equal(o$quiet, FALSE)
+  expect_equal(o$verbose, FALSE)
+})
+
 test_that("opts() fetches options by name if provided", {
   expect_silent(o <- opts(env = "options.example"))
   expect_s3_class(o, "options_list")
   expect_true(length(o) > 2)
   expect_true(length(o) == length(unique(names(o))))
+})
+
+test_that("opts() throws error when mixing named and unnamed list values", {
+  expect_error(opts(list(env = "options.example", "quiet")))
 })
